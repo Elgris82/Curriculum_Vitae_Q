dc469d3d7c00ec744d3fa84794c16b26e62a0931
Sebas <siglointech@gmail.com>
2023-02-27 20:15:43 -0300

    version-0.1

---
+++ CVQ.js
@@ -0,0 +1,14 @@
+function Experiencia(){
+    document.getElementById("continuum").innerHTML=("Q aparece en 12 episodios de la serie star trek")
+    }
+    
+    function Educacion(){
+        document.getElementById("continuum").innerHTML=("Comence mis estudios antropologicos con la base de datos de la nave Enterprise y culmine los mismos con honores en la Federacion Unida de planetas")
+        }
+        function Contacto(){
+            document.getElementById("continuum").innerHTML=("Al ser un dios, me puedes ubicar con el pensamiento...Atiendo a voluntad ")
+        }
+
+        function Informacion(){
+            document.getElementById("continuum").innerHTML=("Nombre: Q - Lugar de aparicion: Espacio continuum Q")
+        }
